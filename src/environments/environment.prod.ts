c9f61727bb32e039b1871287eea5d668bb562e2a
jon <jonnox139@gmail.com>
2022-08-23 20:16:39 -0500

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://apic.mob.sandbox.transfiya.com.co/ext/sandbox/api/',
+  baseUrlDispersions: 'https://apic.mob.sandbox.transfiya.com.co/ext/sandbox/v2/transfiya/operations/web/DispersionOfFunds/'
+};
