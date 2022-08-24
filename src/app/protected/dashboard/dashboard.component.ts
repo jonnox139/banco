fbe5f25927488eaf4080a88e904a078d1a5b0037
steelpwner <molinaestren@gmail.com>
2022-08-24 07:44:03 -0500

    se hace que se pueda deseleccionar una cuenta

--- src/app/protected/dashboard/dashboard.component.ts
+++ src/app/protected/dashboard/dashboard.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { MatRadioChange } from '@angular/material/radio';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { AuthService } from '../../auth/services/auth.service';
+import {
+  MatCheckboxChange,
+  MatCheckboxModule,
+} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
   styles: [
     `
       * {
@@ -29,39 +34,48 @@ export class DashboardComponent implements OnInit {
   flowWeb = false;
   flowApproved = false;
   operacionSeleccionada = null;
+  oldValue = null;
+
   get usuario() {
     return this.authService.usuario;
   }
 
   ngOnInit() {
+    this.cuentas = this.cuentas.map((e) => {
+      return { ...e, selected: false };
+    });
   }
 
   constructor(
     private router: Router,
-    private authService: AuthService,
-    private activeRoute: ActivatedRoute
+    private authService: AuthService
   ) {}
 
-  radioChange() {
-    var obj = this.cuentas.filter(
-      (x) => x.numerocuenta == this.operacionSeleccionada
-    )[0];
+  toggleChecks(e: MatCheckboxChange) {
+    var obj = this.cuentas.filter((x) => x.numerocuenta == e.source.name)[0];
     if (obj) {
-      this.selectedAccount = obj;
+      if (e.checked) {
+        this.selectedAccount = obj;
+        this.cuentas = this.cuentas.map((e2) => ({
+          ...e2,
+          selected: e2.numerocuenta === e.source.name,
+        }));
+      } else {
+        this.selectedAccount = null;
+        this.cuentas = this.cuentas.map((e2) => ({ ...e2, selected: false }));
+      }
     }
   }
 
   navAutorizacionTX() {
-    localStorage.setItem('tx_ref', '')
+    localStorage.setItem('tx_ref', '');
     if (this.selectedAccount !== null) {
       localStorage.setItem(
         'selectedAccount',
         JSON.stringify(this.selectedAccount)
       );
       if (this.flowMovil) {
-        this.router.navigateByUrl(
-          `/verification/authorize-consent`
-        );
+        this.router.navigateByUrl(`/verification/authorize-consent`);
       } else {
         this.router.navigateByUrl(`/verification/authorize-consent`);
       }
